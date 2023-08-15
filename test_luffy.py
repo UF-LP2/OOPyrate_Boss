bf7818a1fca36ccbc810f1e09f65495e91e3cbac
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2023-08-15 03:39:22 -0300

    Update test_luffy.py

--- test_luffy.py
+++ test_luffy.py
@@ -13,22 +13,19 @@ def test_with_file():
         ...
     else:
         myShips: list[any] = []
-        header: bool = True
+        next(ShipLists)
         for ship in ShipLists:
-            if not header:
-                try:
-                    if ship[2] == '' and ship[3] == '':
-                        myShips.append(Ship(int(ship[0]), int(ship[1])))
-                    elif ship[3] == '':
-                        myShips.append(Cruise(int(ship[2]), int(ship[0]), int(ship[1])))
-                    else:
-                        if ship[2] == '':
-                            continue
-                        myShips.append(Cargo(int(ship[2]), float(ship[3]), int(ship[0]), int(ship[1])))
-                except ValueError:
-                    continue
-            else:
-                header = False
+            try:
+                if ship[2] == '' and ship[3] == '':
+                    myShips.append(Ship(int(ship[0]), int(ship[1])))
+                elif ship[3] == '':
+                    myShips.append(Cruise(int(ship[2]), int(ship[0]), int(ship[1])))
+                else:
+                    if ship[2] == '':
+                        continue
+                    myShips.append(Cargo(int(ship[2]), float(ship[3]), int(ship[0]), int(ship[1])))
+            except ValueError:
+                continue
         with pytest.raises(ValueError):
             for ship_ in myShips:
                 assert (ship_.is_worth_it() >= 20) == True
